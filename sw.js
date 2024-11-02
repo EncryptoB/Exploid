2cb8896071009bf0581d3a3802d39ca73b85616a
EncryptoB <57913890+EncryptoB@users.noreply.github.com>
2024-11-02 14:49:41 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'exploid-v1';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(assetsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
